9c228bb1f9ea770ea1b5728bf8d999b5066bba09
OttoHung <totsi04@gmail.com>
2022-05-25 12:47:30 +1000

    Adds Blue, Green and Yellow colour

--- packages/ts-utils/src/log.ts
+++ packages/ts-utils/src/log.ts
@@ -1,6 +1,19 @@
-/* eslint-disable no-unused-vars */
 export enum TextColour{
-    Default = '\x1b[0m',
-    Red = '\x1b[31m',
-    White = '\x1b[0m',
+    Blue    = "\x1b[34m",
+    Default = "\x1b[0m",
+    Green   = "\x1b[32m",
+    Red     = "\x1b[31m",
+    White   = "\x1b[0m",
+    Yellow  = "\x1b[33m",
 }
+
+/* Comment for further reference
+Black        0;30     Dark Gray     1;30
+Red          0;31     Light Red     1;31
+Green        0;32     Light Green   1;32
+Brown/Orange 0;33     Yellow        1;33
+Blue         0;34     Light Blue    1;34
+Purple       0;35     Light Purple  1;35
+Cyan         0;36     Light Cyan    1;36
+Light Gray   0;37     White         1;37
+*/
